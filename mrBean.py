ba6720760711355978cd5ffc0f3241d7d63588b7
Ziming Wang <zwang@atlassian.com>
2017-10-08 16:42:38 +1100

    add lcd test

---
+++ mrBean.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
+
+# Connect infrared and touch sensors to any sensor ports
+# and check they are connected.
+
+ir = InfraredSensor()
+btn = Button();
+mA = LargeMotor('outA')
+assert ir.connected, "Connect a single infrared sensor to any sensor port"
+
+# Put the infrared sensor into proximity mode.
+ir.mode = 'IR-PROX'
+
+while True:    # Stop program by pressing touch sensor button
+    # Infrared sensor in proximity mode will measure distance to the closest
+    # object in front of it.
+    if btn.any():
+        Sound.beep().wait()  # Wait for the beep to finish.
+        #make sure left led is green before exiting
+        Leds.set_color(Leds.LEFT, Leds.GREEN)
+        mA.stop(stop_action="coast")
+        exit()  # Stop the program.
+
+    distance = ir.value()
+
+    if distance < 30:
+        Leds.set_color(Leds.LEFT, Leds.RED)
+        mA.run_forever(speed_sp=200)
+    else:
+        Leds.set_color(Leds.LEFT, Leds.GREEN)
+        mA.stop(stop_action="coast")
+
+
+
