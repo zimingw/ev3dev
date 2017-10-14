fb7a52ad67d78e1466e4710bdb2112df746e202d
Ziming Wang <zwang@atlassian.com>
2017-10-14 17:07:26 +1100

    add color sensor

--- mrBean.py
+++ mrBean.py
@@ -5,7 +5,7 @@ from ev3dev.ev3 import *
 # and check they are connected.
 
 ir = InfraredSensor()
-btn = Button();
+btn = Button()
 mA = MediumMotor('outA')
 cs = ColorSensor()
 assert ir.connected, "Connect a single infrared sensor to any sensor port"
@@ -13,27 +13,27 @@ assert cs.connected, "Connect a color sensor to any sensor port"
 
 # Put the infrared sensor into proximity mode.
 ir.mode = 'IR-PROX'
+cs.mode = 'COL-COLOR'
 
-while True:    # Stop program by pressing touch sensor button
+colors = ('unknown', 'black', 'blue', 'green', 'yellow', 'red', 'white', 'brown')
+
+while True:  # Stop program by pressing touch sensor button
     # Infrared sensor in proximity mode will measure distance to the closest
     # object in front of it.
     if btn.any():
         Sound.beep().wait()  # Wait for the beep to finish.
-        #make sure left led is green before exiting
+        # make sure left led is green before exiting
         Leds.set_color(Leds.LEFT, Leds.GREEN)
         mA.stop(stop_action="coast")
         exit()  # Stop the program.
 
     distance = ir.value()
     color = cs.value()
-    print("Color=" + str(color))
 
     if distance < 25:
         Leds.set_color(Leds.LEFT, Leds.RED)
         mA.run_forever(speed_sp=-200)
+        print("Color=" + colors[color])
     else:
         Leds.set_color(Leds.LEFT, Leds.GREEN)
         mA.stop(stop_action="coast")
-
-
-
